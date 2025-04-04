fcba7bb500ffa3d73a3b338a233f8e67646ae264
mmusstafaa <mustfaa9927@gmail.com>
2025-04-04 15:38:56 +0700

    menambahkan file baru

---
+++ kuliah/pertemuan 10/latihan1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Kuliah</title>
+</head>
+<body>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <tr>Gambar</tr>
+      <tr>NRPr</tr>
+      <tr>Nama</tr>
+      <tr>Email</tr>
+      <tr>Jurusan</tr>
+      <tr>Aksi</tr>
+    </tr> 
+
+  </table>
+</body>
+</html>
